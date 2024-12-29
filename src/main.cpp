c0895c8bc49ae302c146e26271ebaabc5066d786
Dima <dimitriusn24@gmail.com>
2024-12-29 17:52:30 +0300

    OpenGL #2

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,41 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
         glfwSetWindowShouldClose(pWindow, GL_TRUE);
 }
 
+
+
 int main(void)
 {
+    GLfloat point[] =
+    {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] =
+    {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    const char* vertex_shader =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;\n"
+        "layout(location = 1) in vec3 vertex_color;\n"
+        "out vec3 color;\n"
+        "void main(){\n"
+        "   color = vertex_color;\n"
+        "   gl_Position = vec4(vertex_position, 1.0f);\n"
+        "}\0";
+
+    const char* fragment_shader =
+        "#version 460\n"
+        "in vec3 color;\n"
+        "out vec4 frag_color;\n"
+        "void main(){\n"
+        "    frag_color = vec4(color, 1.0f);\n"
+        "}\0";
 
     /* Initialize the library */
     if (!glfwInit())
@@ -55,7 +88,50 @@ int main(void)
     std::cout << "Randerer: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(0, 1, 0, 1);
+    glClearColor(1, 1, 0, 1);
+
+    /* Создание идентификатора для вертексного ШЕЙДЕРА */
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    /* Создание идентификатора для фрагментного ШЕЙДЕРА */
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    /* Создание и настройка шейдерной программы */
+    GLuint shader_programm = glCreateProgram();
+    glAttachShader(shader_programm, vs);
+    glAttachShader(shader_programm, fs);
+    glLinkProgram(shader_programm);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    /* Создание идентификатора для вершинного( вертексного ) буфера */
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); // команда выполняется с текущим буфером
+
+    /* Создание идентификатора для фрагментного буфера */
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
@@ -63,13 +139,16 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader_programm);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteProgram(shader_programm);
     glfwTerminate();
     return 0;
 }
