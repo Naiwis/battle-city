dc299e42fb87bd4d42e11bc1ee192ee8275cec61
Dima <dimitriusn24@gmail.com>
2025-01-04 19:43:53 +0300

    OpenGL #11

---
+++ src/Renderer/Sprite.cpp
@@ -0,0 +1,33 @@
+#include "Sprite.h"
+
+namespace Renderer
+{
+	Sprite::Sprite(const std::shared_ptr<Texture2D> pTexture, const std::shared_ptr<ShaderProdram> pShaderProdram,
+		const glm::vec2& position, const glm::vec2& size, const float rotation)
+	{
+
+	}
+
+	Sprite::~Sprite()
+	{
+
+	}
+
+	void Sprite::render() const
+	{
+
+	}
+	void Sprite::setPosition(const glm::vec2& position)
+	{
+
+	}
+	void Sprite::setSize(const glm::vec2& size)
+	{
+
+	}
+	void Sprite::setRotation(const float rotation)
+	{
+
+	}
+
+}
--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -22,6 +22,7 @@ public:
 
 	std::shared_ptr <Renderer::ShaderProgram> loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath);
 	std::shared_ptr <Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
+
 	std::shared_ptr <Renderer::Texture2D> loadTexture(const std::string& textureName, const std::string& texturePath);
 	std::shared_ptr <Renderer::Texture2D> getTexture(const std::string& textureName);
 
